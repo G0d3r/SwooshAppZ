752ae46111b430e5797f10731c4fff09589eafff
G0d3r <ernest.godlewski@icloud.com>
2019-02-03 12:09:54 +0100

    Initial Commit

---
+++ SwooshAppZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwooshAppZ
+//
+//  Created by ErnestG on 03/02/2019.
+//  Copyright © 2019 EG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
